3458feb8c4e988bf8625f85fa8f8daa71971fa60
LucasHHRossi <lukinharossibr12@gmail.com>
2019-03-22 20:41:14 -0300

    Implantação

---
+++ Exerecicio10_OO/Proj.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerecicio10_OO
+{
+    public class Class1
+    {
+#region Propriedades
+        float salario { get; set;}
+        int numero_filhos { get; set; }         
+        String nome { get; set; }
+#endregion
+    }
+
+    public override string ToString()
+    {
+        return "Salario :" +this.salario + "\n Numero Filhos"+ this.ToString
+    }               
+}
